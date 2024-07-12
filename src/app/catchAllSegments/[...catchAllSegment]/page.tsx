1dda6f71c856443c7ff4e9c37a43c0868a904740
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-07-12 13:23:46 +0530

    final: Catch-all Segments of Route

--- src/app/catchAllSegments/[...catchAllSegment]/page.tsx
+++ src/app/catchAllSegments/[...catchAllSegment]/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function CatchAllSegment() {
-  return <div>...catchAllSegment</div>;
+export default function CatchAllSegment({ params }: any) {
+  console.log(params);
+
+  return (
+    <>
+      <div>1st Route : {params?.catchAllSegment[0]}</div>
+      <div>2nd Route : {params?.catchAllSegment[1]}</div>
+      <div>3rd Route : {params?.catchAllSegment[2]}</div>
+      <div>4th Route : {params?.catchAllSegment[3]}</div>
+      <div>5th Route : {params?.catchAllSegment[4]}</div>
+    </>
+  );
 }
