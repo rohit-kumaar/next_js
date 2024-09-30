9d4cfd5a4316058abd5ff3e0573b58158ac3dced
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-09-30 15:41:57 +0530

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import "../../src/app/style.css";
+import { API_BASE_URL } from "@/config/constants";
 
 export default function Home() {
+  console.log(process.env.SERVER_PASSWORD);
+
   return (
     <main>
-      <h1>Rohit Kumar</h1>
+      {process.env.NODE_ENV == "development" ? (
+        <h1>You are in development mode</h1>
+      ) : (
+        <h1>You are on production mode</h1>
+      )}
+
+      {API_BASE_URL}
     </main>
   );
 }
