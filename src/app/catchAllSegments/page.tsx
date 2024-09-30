52e00402036637571bce364a5729880b84b924f1
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2024-09-30 14:18:32 +0530

    blank page

--- src/app/catchAllSegments/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CatchAllSegments() {
-  return <div>catchAllSegments</div>;
-}
--- src/app/dynamic-route/[dynamicRoutePage]/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-export default function DynamicRoutePage({ params }: any) {
-  console.log(params);
-
-  return <div>Test Dynamic Router Components : {params.dynamicRoutePage}</div>;
-}
